0258d9651182581d87a2a4df02cff51d67328175
sosoesun <sosoesun@naver.com>
2024-01-25 23:21:42 +0900

    aa

--- 10163.py
+++ 10163.py
@@ -1,7 +1,8 @@
+import sys
 
 def paper_info(paper,k):
 
-    x_start, y_start, length, height = map(int,input().split())
+    x_start, y_start, length, height = map(int,sys.stdin.readline().split())
     for i in range(length):
         for j in range(height):
             paper[(x_start+i)*1001+y_start+j] = k
@@ -9,12 +10,11 @@ def paper_info(paper,k):
     return paper
 
 
-def paper_count(paper,k):
+def paper_count(paper2,count2,k):
 
     count = 0
-    for i in range(1001):
-        for j in range(1001):
-            if paper[1001*i+j] == k:
+    for i in range(count2):
+        if paper2[i] == k:
                 count += 1
 
     return count   
@@ -25,13 +25,22 @@ def main():
     paper = [-1 for i in range(1001*1001)]
     
     # 색종이 수를 입력받고 색종이 정보를 입력한다.
-    number = int(input())
+    number = int(sys.stdin.readline())
     for i in range(number):
         paper = paper_info(paper,i)
+    
+    #옮겨담기
+    count2 = 0
+    paper2 = []
+    for i in range(1001):
+        for j in range(1001):
+            if paper[1001*i+j] != -1:
+                count2 += 1
+                paper2.append(paper[1001*i+j])
 
     #종이 면적 카운트
     for i in  range(number):
-        print(paper_count(paper,i))
+        print(paper_count(paper2,count2,i))
 
 
 if __name__ == "__main__":
